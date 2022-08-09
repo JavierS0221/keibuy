c8764fcd4418c489fe2bf7419e8e862ba28dbc3c
JavierS0221 <javiers.v0221@gmail.com>
2022-08-09 11:55:09 -0300

    Revert "innital commit"
    This reverts commit dfdc8a4d5e6cb7cb1cc40613f085e46c83228737.

--- src/main/java/com/project/auction/dao/ReportDao.java
+++
@@ -1,8 +0,0 @@
-package com.project.auction.dao;
-
-import com.project.auction.model.Report;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReportDao extends CrudRepository<Report, Long> {
-
-}
