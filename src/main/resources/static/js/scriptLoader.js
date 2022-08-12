18623152a58df6b52982b5a8d37ae35491eaeb03
JavierS0221 <javiers.v0221@gmail.com>
2022-08-12 15:48:20 -0300

    arreglos en animación de carga

--- src/main/resources/static/js/scriptLoader.js
+++ src/main/resources/static/js/scriptLoader.js
@@ -1,5 +1,11 @@
 window.addEventListener('load', () => {
+    sleep(2000).then(()=>{
     const loaderContainer = document.querySelector('.loader-container');
     loaderContainer.style.opacity = 0;
     loaderContainer.style.visibility = 'hidden'
+    })
 })
+
+function sleep(ms){
+    return new Promise( resolver => setTimeout(resolver, ms));
+};
