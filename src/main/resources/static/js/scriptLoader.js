848049166075eb81bfbc64ca91f0a522f55dca22
JavierS0221 <javiers.v0221@gmail.com>
2022-08-12 15:42:31 -0300

    animación de carga intento 2

--- src/main/resources/static/js/scriptLoader.js
+++ src/main/resources/static/js/scriptLoader.js
@@ -1,5 +1,5 @@
-// window.addEventListener('load', () => {
-//     const loaderContainer = document.querySelector('.loader-container');
-//     loaderContainer.style.opacity = 0;
-//     loaderContainer.style.visibility = 'hidden'
-// })
+window.addEventListener('load', () => {
+    const loaderContainer = document.querySelector('.loader-container');
+    loaderContainer.style.opacity = 0;
+    loaderContainer.style.visibility = 'hidden'
+})
