23db2d78265648baab52f706734831de0a60fa11
JavierS0221 <javiers.v0221@gmail.com>
2022-08-12 15:40:44 -0300

    animación de carga intento 1

--- src/main/java/com/project/auction/web/SecurityConfig.java
+++ src/main/java/com/project/auction/web/SecurityConfig.java
@@ -12,6 +12,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/img/**").permitAll();
+                .antMatchers("/", "/img/**", "/js/**", "/css/**").permitAll();
     }
 }
---
+++ src/main/resources/static/js/scriptLoader.js
@@ -0,0 +1,5 @@
+// window.addEventListener('load', () => {
+//     const loaderContainer = document.querySelector('.loader-container');
+//     loaderContainer.style.opacity = 0;
+//     loaderContainer.style.visibility = 'hidden'
+// })
