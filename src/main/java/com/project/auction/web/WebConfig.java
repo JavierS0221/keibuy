224448e42e389a7291fddd4568e974005b133336
Alex <alexisborges235@gmail.com>
2022-08-26 01:13:37 +0200

    Añadido perfil y español en barra de herramientas

--- src/main/java/com/project/auction/web/WebConfig.java
+++ src/main/java/com/project/auction/web/WebConfig.java
@@ -43,6 +43,7 @@ public class WebConfig implements WebMvcConfigurer{
         registry.addViewController("/profile").setViewName("/pages/profile");
         registry.addViewController("/errors/403").setViewName("/errors/403");
         registry.addViewController("/email").setViewName("email");
+        registry.addViewController("/perfil").setViewName("/pages/profile");
     }
 
 }
